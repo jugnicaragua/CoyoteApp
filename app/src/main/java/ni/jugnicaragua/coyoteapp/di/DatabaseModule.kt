07beda4c9209b1af4ca57419c3c03f14f6d34220
Luis Cardoza Bird <luisalfonsocb83@gmail.com>
2020-03-09 13:07:59 -0600

    Finished Sealed Class to handle result

--- app/src/main/java/ni/jugnicaragua/coyoteapp/di/DatabaseModule.kt
+++ app/src/main/java/ni/jugnicaragua/coyoteapp/di/DatabaseModule.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import androidx.room.Room
 import ni.jugnicaragua.coyoteapp.data.local.database.CoyoteDb
 import ni.jugnicaragua.coyoteapp.data.local.database.dao.BanksDao
+import ni.jugnicaragua.coyoteapp.data.local.database.dao.CentralBankDao
+import ni.jugnicaragua.coyoteapp.data.local.database.dao.ExchangeRateDao
 import org.koin.android.ext.koin.androidApplication
 import org.koin.dsl.module
 
@@ -16,11 +18,20 @@ val databaseModule = module {
             .build()
     }
 
-
     fun provideBanksDao(database: CoyoteDb): BanksDao {
         return database.banksDbTableDao()
     }
 
+    fun provideExchangeRateDao(database: CoyoteDb): ExchangeRateDao {
+        return database.exchangeRateDbTableDao()
+    }
+
+    fun provideCentralBankDao(database: CoyoteDb): CentralBankDao {
+        return database.centralBankDbTableDao()
+    }
+
     single { provideDatabase(androidApplication()) }
-    single { provideDao(get()) }
+    single { provideBanksDao(get()) }
+    single { provideExchangeRateDao(get()) }
+    single { provideCentralBankDao(get()) }
 }
